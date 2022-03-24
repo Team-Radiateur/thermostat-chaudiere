4b17379a2a25dd3fc28f72e2e4b461e3a3a64ddb
Christophe 'DrakeXorn' Bernard <drakexorn@gmail.com>
2022-03-25 00:50:06 +0100

    [ADD] Thermostat-Chaudière: set the bot activity

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -1,10 +1,16 @@
 import { logger } from "../../helpers/logger";
 import { DiscordEvent } from "../types/discordEvents";
+import { DiscordClient } from "../types/discordClient";
 
 module.exports = {
 	name: "ready",
 	once: true,
 	execute: () => {
 		logger.info("La Chaudière est connectée au Radiateur, le circuit d'eau ne fuite pas !");
+
+		setInterval(() => {
+			const temperature = Math.floor(Math.random() * 70 + 21);
+			DiscordClient.getInstance().user?.setActivity(`Température de la Chaudière : ${temperature}°C`);
+		}, 5000);
 	}
 } as DiscordEvent;
