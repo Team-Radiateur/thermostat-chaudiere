110317749e05911b8d2d977741d428d75e0f6bd1
Christophe 'DrakeXorn' Bernard <drakexorn@gmail.com>
2022-04-24 22:06:27 +0200

    [WIP] Thermostat-Chaudière: manage presence updates

--- src/bot/events/presenceUpdate.ts
+++ src/bot/events/presenceUpdate.ts
@@ -1,13 +1,35 @@
 import { DiscordEvent } from "../types/discordEvents";
-import { Presence } from "discord.js";
+import { MessageEmbed, Presence } from "discord.js";
+import { bold } from "@discordjs/builders";
+import { DiscordClient } from "../types/discordClient";
+import { env } from "../../../config/env";
 
 const presenceUpdate: DiscordEvent = {
 	name: "presenceUpdate",
 	once: false,
 	execute: async (oldPresence: Presence | undefined, newPresence: Presence) => {
-		oldPresence;
-		newPresence;
-		//console.log("Presence update detected");
+		if (oldPresence?.user?.bot) return;
+
+		const loggingChannel = DiscordClient.getInstance().channels.cache.get(env.bot.userUpdateLoggingChannel);
+		if (!loggingChannel || !loggingChannel.isText()) return;
+		const embed = new MessageEmbed()
+			.setTitle(bold(`${oldPresence?.user?.tag} a mis à jour son profil !`))
+			.setColor("#00ffff")
+			.setFooter({ text: `ID de l'utilisateur: ${newPresence.userId} • ${new Date().toLocaleString("fr")}` })
+			.setAuthor({ name: newPresence.user?.tag || "", iconURL: newPresence.user?.displayAvatarURL() });
+
+		if (
+			!oldPresence?.activities?.every(
+				(activity, index) => activity.type !== newPresence?.activities?.[index]?.type
+			)
+		) {
+			embed.addField(
+				"Activité",
+				`${oldPresence?.activities?.[0]?.type} => ${newPresence?.activities?.[0]?.type}`
+			);
+		}
+
+		loggingChannel.send({ embeds: [embed] });
 	}
 };
 
--- src/bot/events/userUpdate.ts
+++ src/bot/events/userUpdate.ts
@@ -1,8 +1,9 @@
-import { DiscordEvent } from "../types/discordEvents";
 import { MessageEmbed, User } from "discord.js";
+import { bold, hyperlink } from "@discordjs/builders";
+
+import { DiscordEvent } from "../types/discordEvents";
 import { DiscordClient } from "../types/discordClient";
 import { env } from "../../../config/env";
-import { hyperlink, userMention } from "@discordjs/builders";
 
 const userUpdate: DiscordEvent = {
 	name: "userUpdate",
@@ -13,8 +14,10 @@ const userUpdate: DiscordEvent = {
 		const loggingChannel = DiscordClient.getInstance().channels.cache.get(env.bot.userUpdateLoggingChannel);
 		if (!loggingChannel || !loggingChannel.isText()) return;
 		let embed = new MessageEmbed()
-			.setTitle(`${userMention(oldUser.id)} a mis à jour son profil !`)
-			.setColor("#00ffff");
+			.setTitle(bold(`${oldUser.tag} a mis à jour son profil !`))
+			.setColor("#00ffff")
+			.setAuthor({ name: oldUser.tag, iconURL: oldUser.displayAvatarURL() })
+			.setFooter({ text: `ID de l'utilisateur: ${newUser.id} • ${new Date().toLocaleString()}` });
 
 		if (oldUser.displayAvatarURL() !== newUser.displayAvatarURL()) {
 			// eslint-disable-next-line max-len
@@ -26,14 +29,16 @@ const userUpdate: DiscordEvent = {
 				)}`
 			);
 		} else if (oldUser.username !== newUser.username) {
-			embed = embed.addField("Nouveau pseudo", newUser.username);
+			embed = embed.addField("Nouveau pseudo", `${oldUser.username} => ${newUser.username}`);
 		} else if (oldUser.discriminator !== newUser.discriminator) {
-			embed = embed.addField("Nouveau discriminant", newUser.discriminator);
+			embed = embed.addField("Nouveau discriminant", `${oldUser.discriminator} => ${newUser.discriminator}`);
 		} else if (oldUser.tag !== newUser.tag) {
 			embed = embed.addField("Nouveau tag", newUser.tag);
+		} else {
+			return;
 		}
 
-		await loggingChannel.send(embed.toString());
+		await loggingChannel.send({ embeds: [embed] });
 	}
 };
 
