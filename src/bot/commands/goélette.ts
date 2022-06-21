1b4002046dcb9fb3f526b5c33c58c449ac9555ba
Christophe 'DrakeXorn' Bernard <drakexorn@gmail.com>
2022-06-22 01:15:17 +0200

    [ADD] Thermostat-Chaudière: Goélette au rapport ! #12

--- src/bot/commands/goélette.ts
+++ src/bot/commands/goélette.ts
@@ -1,19 +1,29 @@
 import { SlashCommandBuilder, userMention } from "@discordjs/builders";
-import { Guild, GuildMember } from "discord.js";
+import { Guild, GuildMember, Role, User } from "discord.js";
 
 import { prepareEmbed, replyToInteraction } from "../../helpers/macros";
 import { DiscordCommand } from "../types/discordEvents";
 
 const goelette: DiscordCommand = {
-	data: new SlashCommandBuilder().setName("goélette").setDescription("Goélette au rapport !") as SlashCommandBuilder,
+	data: new SlashCommandBuilder()
+		.setName("goélette")
+		.setDescription("Goélette au rapport !")
+		.addMentionableOption(option =>
+			option
+				.setName("personne")
+				.setDescription("La personne à mentionner (si pas fourni, mentionne une personne aléatoirement)")
+				.setRequired(false)
+		) as SlashCommandBuilder,
 	execute: async interaction => {
 		const description = "Goélette au rapport !";
 		const embed = prepareEmbed(interaction.user).setTitle("Valve thermostatique textuelle");
-		const randomGuildUser = (interaction.guild as Guild).members.cache.random() as GuildMember;
+		const guildMember =
+			(interaction.options.getMentionable("personne") as User | GuildMember | Role | null) ||
+			((interaction.guild as Guild).members.cache.random() as GuildMember);
 
 		return await replyToInteraction(
 			interaction,
-			embed.setDescription(`${description} ${userMention(randomGuildUser.id)}`),
+			embed.setDescription(`${description} ${userMention(guildMember.id)}`),
 			false
 		);
 	}
