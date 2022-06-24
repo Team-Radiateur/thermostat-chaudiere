bb03f62eb0909dc108156bbc7870306b356f3ffc
Christophe 'DrakeXorn' Bernard <drakexorn@gmail.com>
2022-06-24 23:29:55 +0200

    [FIX] Thermostat-Chaudière: Tag user in message instead of embed for goélette command

--- src/bot/commands/goélette.ts
+++ src/bot/commands/goélette.ts
@@ -1,7 +1,7 @@
-import { SlashCommandBuilder, userMention } from "@discordjs/builders";
+import { roleMention, SlashCommandBuilder, userMention } from "@discordjs/builders";
 import { Guild, GuildMember, Role, User } from "discord.js";
 
-import { prepareEmbed, replyToInteraction } from "../../helpers/macros";
+import { prepareEmbed } from "../../helpers/macros";
 import { DiscordCommand } from "../types/discordEvents";
 
 const goelette: DiscordCommand = {
@@ -15,17 +15,15 @@ const goelette: DiscordCommand = {
 				.setRequired(false)
 		) as SlashCommandBuilder,
 	execute: async interaction => {
-		const description = "Goélette au rapport !";
 		const embed = prepareEmbed(interaction.user).setTitle("Valve thermostatique textuelle");
 		const guildMember =
 			(interaction.options.getMentionable("personne") as User | GuildMember | Role | null) ||
 			((interaction.guild as Guild).members.cache.random() as GuildMember);
 
-		return await replyToInteraction(
-			interaction,
-			embed.setDescription(`${description} ${userMention(guildMember.id)}`),
-			false
-		);
+		return await interaction.reply({
+			content: guildMember instanceof GuildMember ? userMention(guildMember.id) : roleMention(guildMember.id),
+			embeds: [embed.setDescription("Goélette au rapport !")]
+		});
 	}
 };
 
