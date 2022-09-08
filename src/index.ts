d85e5a38ed6ac15d3de81ac760efd3e8e06008c2
Christophe 'DrakeXorn' Bernard <drakexorn@gmail.com>
2022-09-08 17:14:13 +0200

    [ADD] Thermostat-Chaudière: log bot starting and exiting

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,19 @@ import startBot from "./bot";
 import { logger } from "./helpers/logger";
 
 const startApp = async (): Promise<void> => {
+	logger.info("Starting the bot...");
 	const botLoadingFinished = await startBot();
 
 	if (!botLoadingFinished) {
 		logger.error("Une erreur est survenue lors de l'initialisation du bot.");
 		process.exit(1);
+	} else {
+		logger.info("Bot started !");
 	}
 };
 
-startApp().then(() => {
-	logger.info("Server ready!");
+startApp();
+
+process.on("beforeExit", () => {
+	logger.info("Stopping the bot...");
 });
